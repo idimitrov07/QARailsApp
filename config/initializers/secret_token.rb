d1eea3ef17db03e7b6525fb81f48b5e19a1194ea
Ivan Dimitrov <idimitrov07@gmail.com>
2013-11-06 20:11:47 +0200

    initial commit: QA rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makeitsnappy::Application.config.secret_token = 'a7a8a04c98fd5600a95585da8bc0dcfeffb6faf3a40b3efaeb36d450db4d3fc12c15a8251f780438e5b85486bf401e15d62b16d4d8c17f8cd9601997afe23fd7'

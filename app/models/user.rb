0291348d57253df2bf5411f95f84fb4433ebc477
Ivan Dimitrov <idimitrov07@gmail.com>
2013-11-07 17:20:03 +0200

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :password_digest, :username
+  attr_accessible :username, :password, :password_confirmation
+
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false},
+  										 length: { in: 4..12 }, 
+  										 format: { with: /^[a-z][a-z0-9]*$/, message: "only lower case letters and numbers" } 
+
+  validates :password, length: { in: 4..8 }
+  validates :password_confirmation, length: { in: 4..8 }
 end
